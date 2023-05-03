35a116bedad47390019da715aecbcbe62e772762
morita-koki <morita82007koki@gmail.com>
2023-05-04 05:56:03 +0900

    バグフィックス

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-const app = require("express")();
+const express = require("express");
+const app = express();
+
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
 const DOCUMENT_ROOT = __dirname + "/public";
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.sendFile(DOCUMENT_ROOT + "/index.html");
